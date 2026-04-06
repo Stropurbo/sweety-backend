a93fead0bc813d989c921dc141397382dac7a3ae
Sabbir Hasan <xofficial490@gmail.com>
2026-04-06 20:10:49 +0600

    superuser add on render

--- build.sh
+++ build.sh
@@ -5,3 +5,15 @@ pip install -r requirements.txt
 pip install gunicorn
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+import os
+email = os.environ.get('SUPERUSER_EMAIL', 'admin@admin.com')
+password = os.environ.get('SUPERUSER_PASSWORD', 'admin123')
+if not User.objects.filter(email=email).exists():
+    User.objects.create_superuser(email=email, password=password, first_name='Admin', last_name='User')
+    print('Superuser created')
+else:
+    print('Superuser already exists')
+"
