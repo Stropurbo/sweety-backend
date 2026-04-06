59e91a4a09e201d20e42e6d9ff2938e62b79600e
Sabbir Hasan <xofficial490@gmail.com>
2026-04-06 20:01:53 +0600

    fix issues

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,26 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+
+@require_http_methods(['GET'])
+def api_root(request):
+    return JsonResponse({
+        'name': 'Sweety API',
+        'version': '1.0.0',
+        'status': 'running',
+        'endpoints': {
+            'auth': '/api/auth/',
+            'posts': '/api/posts/',
+            'friends': '/api/friends/',
+            'notifications': '/api/notifications/',
+        }
+    })
 
 urlpatterns = [
+    path('', api_root),
+    path('api/', api_root),
     path('admin/', admin.site.urls),
     path('api/auth/', include('users.urls')),
     path('api/posts/', include('posts.urls')),
